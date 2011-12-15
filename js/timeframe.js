9d2184b7941ba9878a06c721fb5d4da4f338c84c
Luis Ubeda <lubeme23@gmail.com>
2011-12-15 19:23:37 +0100

    icons for timeLapse added

--- js/timeframe.js
+++ js/timeframe.js
@@ -5,12 +5,17 @@ $(document).ready(function() {
 	$('#to').datetimepicker({
 		minDate: new Date(2011, 11, 1, 8, 30), maxDate: new Date(2011, 11, 31, 17, 30)
 		});
-	//$('#menu').hide();
+	$('#menu').hide();
+	$('#timeFrameButton').click(function() {
+		$('#menu').slideDown('slow', function() {
+		// Animation complete.
+		});
+	});
 });
 
 function conversion(){
-	var from = new Date(document.getElementById("from").value)//.getTime();
-    var to = new Date(document.getElementById("to").value)//.getTime();
+	var from = new Date(document.getElementById("from").value);
+    var to = new Date(document.getElementById("to").value);
     var fromEpoch = Math.round(from.getTime()/1000);
     var toEpoch = Math.round(to.getTime()/1000);
 
@@ -19,8 +24,4 @@ function conversion(){
     
 }
 
-$('#test').click(function() {
-  $('#menu').slideDown('slow', function() {
-    // Animation complete.
-  });
-});
+
