e4db5fcc15c5b626c5dcbbfa60df696ef179cb24
Luis Ubeda <lubeme23@gmail.com>
2012-02-27 12:40:17 +0100

    Interface improvements. Dialog for options created.

--- js/timeframe.js
+++ js/timeframe.js
@@ -1,6 +1,25 @@
 var menuShown = false;
 
 $(document).ready(function() {
+	$( "#dialog:ui-dialog" ).dialog( "destroy" );
+	$( "#menu" ).dialog({
+			autoOpen: false,
+			height: 300,
+			width: 280,
+			modal: true,
+			buttons: {
+				"Submit": function() {
+					$("#timeFrameData").submit();
+				},
+				Cancel: function() {
+					$(this).dialog( "close" );
+				}
+			}
+		});
+		
+	$('#timeFrameButton').click(function() {
+		$( "#menu" ).dialog( "open" );
+	});
 	$('#from').datetimepicker({
 		minDate: new Date(2011, 11, 1, 8, 30),
 		maxDate: new Date()
@@ -8,53 +27,19 @@ $(document).ready(function() {
 	$('#to').datetimepicker({
 		minDate: new Date(2011, 11, 1, 8, 30),
 		maxDate: new Date()
-		});
-	$('#menu').hide();
-	$('#timeFrameButton').click(function() {
-		var menuPosition = $('#timeFrameButton').offset();
-		menuPosition.left += 70;
-		menuPosition.top += 40;
-		menuPosition.top -= $('#menu').outerHeight();
-		$('#menu').css(menuPosition);
-		$('#menu').fadeToggle('slow');
-		/*
-		
-		if (!menuShown){
-			$('#menu').slideDown('slow', function() {
-		        menuShown=true;
-		    });
-			
-		}else{
-			$('#menu').slideUp('slow', function() {
-		// Animation complete.
-		});
-			menuShown=false;
-		}*/
-		
-	});
+		});	
 });
 
 function conversion(){
-	
-	var fromString = document.getElementById("from").value
-	var toString = document.getElementById("to").value
+	var fromString = $("#from").val()
+	var toString = $("#to").val()
 	if (fromString != "" && toEpoch != ""){
 		var from = new Date(fromString);
 		var to = new Date(toString);
-		
 		var fromEpoch = Math.round(from.getTime()/1000);
 		var toEpoch = Math.round(to.getTime()/1000);
-		document.timeFrameData.from.value = fromEpoch;
-		document.timeFrameData.to.value = toEpoch;
-		document.timeFrameData.timeFrameValue.value="true";
-	   
-	}
-	
-    
-    
-
-    
-    
+		$("#from").val(fromEpoch.toString());
+		$("#to").val(toEpoch.toString());
+		$("#timeFrameValue").val("true");  
+	}   
 }
-
-
